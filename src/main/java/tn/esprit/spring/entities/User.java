6302b9b5cc504b0249591597e417e69dcacca371
nadianbh <115391369+nadianbh@users.noreply.github.com>
2024-10-06 19:25:09 +0200

    first

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -20,7 +20,7 @@ public class User implements Serializable {
 
 
 	private static final long serialVersionUID = 1L;
-
+//comm
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;   
